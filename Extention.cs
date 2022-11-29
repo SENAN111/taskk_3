006d673d1af14e2b3cfa8f5befc0f4804e0fecb8
SENAN111 <behbudovsenan0@gmail.com>
2022-11-30 00:28:27 +0400

    first

---
+++ Extention.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    public static class Extention
+    {
+        public static bool IsOdd(this int number)
+        {
+            return number % 2 == 0;
+        }
+        public static bool IsEven(this int number)
+        {
+            return number % 2 == 1;
+        }
+        public static bool HasDigit(this string text)
+        {
+            for (int i = 0; text.Length > i; i++)
+            {
+                if (char.IsDigit(text[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static string Capitalize(this string text_2)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(char.ToUpper(text_2[0]));
+            sb.Append(text_2.Substring(1).ToLower());
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp16;
+
+namespace Extension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            string text= Console.ReadLine();
+            string text_2= Console.ReadLine();
+            Console.WriteLine(Extention.IsOdd(number));
+            Console.WriteLine(Extention.IsOdd(number));
+            Console.WriteLine(Extention.HasDigit(text));
+            Console.WriteLine(Extention.Capitalize(text_2));
+        }
+    }
+}
